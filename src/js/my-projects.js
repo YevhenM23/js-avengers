fdb3c881ff5c6699d9bc349896fe2538bb3a2e23
ABurianenko <121970095+ABurianenko@users.noreply.github.com>
2024-11-20 00:51:44 +0000

    create markup and button's action

---
+++ src/js/my-projects.js
@@ -0,0 +1,37 @@
+`use strict`
+
+const projects = document.querySelectorAll(".project-card");
+const btnLoadMore = document.querySelector(".js-load-more");
+const btnShowLess = document.querySelector(".js-show-less");
+
+let visibleProjects = 3;
+
+function showProjects() {
+    projects.forEach((project, index) => {
+        if (index < visibleProjects) {
+            project.classList.add('visible')
+        } else {
+            project.classList.remove('visible')
+        }
+    });
+
+    if (visibleProjects >= projects.length) {
+        btnLoadMore.style.display = 'none';
+        btnShowLess.style.display = 'block';
+    } else {
+        btnLoadMore.style.display = 'block';
+        btnShowLess.style.display = 'none';
+    }
+}
+
+btnLoadMore.addEventListener("click", () => {
+    visibleProjects += 3;
+    showProjects();
+});
+
+btnShowLess.addEventListener("click", () => {
+    visibleProjects = 3;
+    showProjects();
+});
+
+showProjects();
