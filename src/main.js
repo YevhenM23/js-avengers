2097a65df94b2f2831b7328a265a2701d9732627
Yevhen Maslov <yevheniimaslov@gmail.com>
2024-12-02 21:05:00 +0200

    fin com

--- src/main.js
+++ src/main.js
@@ -6,3 +6,18 @@ import './js/faq.js';
 import './js/reviews.js';
 import './js/work-together.js';
 import './js/tech-skills.js';
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/js-avengers/', 
+    build: {
+      rollupOptions: {
+      external: ['fsevents'],
+    },
+    outDir: 'dist'
+    }
+})
+
+
+
